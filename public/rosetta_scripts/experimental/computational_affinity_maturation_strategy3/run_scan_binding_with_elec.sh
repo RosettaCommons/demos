7b9a6cf6101f24ee41f015e77d4b5cd8c5a1b49d
Sachko Honda <honda@apl.washington.edu>
2012-12-17 22:47:51 +0000

    Initial check-in of cache-enabled PB energy.  No numerical diff expected.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/rosetta_demos@52719 6189c7af-7406-0410-a2ce-f22f51069c23

--- public/rosetta_scripts/experimental/computational_affinity_maturation_strategy3/run_scan_binding_with_elec.sh
+++ public/rosetta_scripts/experimental/computational_affinity_maturation_strategy3/run_scan_binding_with_elec.sh
@@ -4,22 +4,17 @@ if [ "$#" -eq 0 ];then
    exit
 fi
 
+rosetta_home=${HOME}/rosetta-trunk
+mode=release
 path_name=$(dirname $1)
 pdb_name=$(basename $1 .pdb)
 first_res=`grep "ATOM.................B" $1|head -1|awk '{print $6}'`
 last_res=`grep "ATOM.................B" $1|tail -1|awk '{print $6}'`
-DB="~/minirosetta_database"
+db=${rosetta_home}/rosetta_database
+rosetta_scripts=${rosetta_home}/rosetta_source/bin/rosetta_scripts.linuxgcc${mode}
 
-rosetta_scripts.linuxiccrelease -database ${DB}  -s ${path_name}/${pdb_name}.pdb  -parser:protocol ppk_minbb_use_correct.xml -ex1 -ex2 -use_input_sc -correct -overwrite
+${rosetta_scripts} -database ${db}  -s ${path_name}/${pdb_name}.pdb  -parser:protocol ppk_minbb_use_correct.xml -ex1 -ex2 -use_input_sc -correct -overwrite
 
-/work/yfsong/SVN/fresh/mini/bin/PB_potential.linuxgccrelease -database ${DB}  -s ${path_name}/${pdb_name}_0001.pdb -correct -parse_charge -nooutput 
+${rosetta_scripts} -dadtabase ${db}  -s ${path_name}/${pdb_name}_0001.pdb  -parser:protocol ppk_minbb_add_elec.xml -ex1 -ex2 -use_input_sc -correct -parse_charge -overwrite
 
-/work/yfsong/dist/pymol/freemol/bin/apbs.exe ${path_name}/${pdb_name}_0001_0001.in
-
-rosetta_scripts.linuxiccrelease -database ${DB}  -s ${path_name}/${pdb_name}_0001.pdb  -parser:protocol ppk_minbb_add_elec.xml -ex1 -ex2 -use_input_sc -correct -parse_charge -PB_potential_file ${path_name}/${pdb_name}_0001_0001.dx -PB_score_residue_range ${first_res} ${last_res} -score:weights score12_w_corrections.wts -score:patch add_PB_elec.wts_patch -overwrite
-
-PB_potential.linuxgccrelease -database ${DB}  -s ${path_name}/${pdb_name}_0001_0001.pdb -correct -parse_charge -nooutput
-
-apbs.exe ${path_name}/${pdb_name}_0001_0001_0001.in
-
-rosetta_scripts.linuxiccrelease -database ${DB}  -s ${path_name}/${pdb_name}_0001_0001.pdb -parser:protocol scan_binding_with_elec.xml   -ex1 -ex2 -use_input_sc -nooutput -correct -parse_charge -PB_potential_file ${path_name}/${pdb_name}_0001_0001_0001.dx -PB_score_residue_range ${first_res} ${last_res} -score:weights score12_w_corrections.wts -score:patch add_PB_elec.wts_patch -overwrite
+${rosetta_scripts} -database ${db}  -s ${path_name}/${pdb_name}_0001_0001.pdb -parser:protocol scan_binding_with_elec.xml   -ex1 -ex2 -use_input_sc -nooutput -correct -parse_charge -overwrite
