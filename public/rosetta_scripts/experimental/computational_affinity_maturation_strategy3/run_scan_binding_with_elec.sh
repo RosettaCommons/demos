f81fd7222c9dfd01d1fdeb7c2c3274e3f3b055c8
Sachko Honda <honda@apl.washington.edu>
2013-01-16 09:30:11 +0000

    PB links to APBS libs via iAPBS when "extras=apbs" is given at compilation.  Functionalized the PB solver: 10% speed up per evaluation.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/rosetta_demos@53175 6189c7af-7406-0410-a2ce-f22f51069c23

--- public/rosetta_scripts/experimental/computational_affinity_maturation_strategy3/run_scan_binding_with_elec.sh
+++ public/rosetta_scripts/experimental/computational_affinity_maturation_strategy3/run_scan_binding_with_elec.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
-if [ "$#" -eq 0 ];then
-   echo "run.sh pdb_file"
-   exit
-fi
+#if [ "$#" -eq 0 ];then
+#   echo "run.sh pdb_file"
+#   exit
+#fi
 
 rosetta_home=${HOME}/rosetta-trunk
-mode=release
-path_name=$(dirname $1)
-pdb_name=$(basename $1 .pdb)
-first_res=`grep "ATOM.................B" $1|head -1|awk '{print $6}'`
-last_res=`grep "ATOM.................B" $1|tail -1|awk '{print $6}'`
+mode=debug
+#pdb_path=$1
+pdb_path=~/pdbs/HB80.3_relax.pdb
+echo input structure=${pdb_path}
+path_name=`dirname ${pdb_path}`
+echo dir=${path_name}
+pdb_name=`basename ${pdb_path} .pdb`
+echo filename=${pdb_name}
 db=${rosetta_home}/rosetta_database
+echo db=${db}
 rosetta_scripts=${rosetta_home}/rosetta_source/bin/rosetta_scripts.linuxgcc${mode}
+echo executable=${rosetta_scripts}
 
-${rosetta_scripts} -database ${db}  -s ${path_name}/${pdb_name}.pdb  -parser:protocol ppk_minbb_use_correct.xml -ex1 -ex2 -use_input_sc -correct -overwrite
+echo I am here
 
-${rosetta_scripts} -dadtabase ${db}  -s ${path_name}/${pdb_name}_0001.pdb  -parser:protocol ppk_minbb_add_elec.xml -ex1 -ex2 -use_input_sc -correct -parse_charge -overwrite
+#${rosetta_scripts} -database ${db}  -s ${path_name}/${pdb_name}.pdb  -parser:protocol ppk_minbb_use_correct.xml -ex1 -ex2 -use_input_sc -correct -overwrite
 
-${rosetta_scripts} -database ${db}  -s ${path_name}/${pdb_name}_0001_0001.pdb -parser:protocol scan_binding_with_elec.xml   -ex1 -ex2 -use_input_sc -nooutput -correct -parse_charge -overwrite
+echo here I am
+
+${rosetta_scripts} -database ${db}  -s ${pdb_name}_0001.pdb  -parser:protocol ppk_minbb_add_elec.xml -ex1 -ex2 -use_input_sc -correct -parse_charge -overwrite
+
+echo here we go
+#${rosetta_scripts} -database ${db}  -s ${path_name}/${pdb_name}_0001_0001.pdb -parser:protocol scan_binding_with_elec.xml   -ex1 -ex2 -use_input_sc -nooutput -correct -parse_charge -overwrite
