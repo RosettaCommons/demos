223dfddc2041536e187deb2897129131944aba6b
Sachko Honda <honda@apl.washington.edu>
2013-01-24 16:41:48 +0000

    No reevaluation of PB in unbound state.  Elimiate the requirement for the obsolete apbs_path parameter.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/rosetta_demos@53317 6189c7af-7406-0410-a2ce-f22f51069c23

--- public/rosetta_scripts/experimental/computational_affinity_maturation_strategy3/run_scan_binding_with_elec.sh
+++ public/rosetta_scripts/experimental/computational_affinity_maturation_strategy3/run_scan_binding_with_elec.sh
@@ -4,8 +4,8 @@
 #   exit
 #fi
 
-rosetta_home=${HOME}/rosetta-trunk
-mode=debug
+rosetta_home=${HOME}/dvlp/rosetta-trunk
+mode=release
 #pdb_path=$1
 pdb_path=~/pdbs/HB80.3_relax.pdb
 echo input structure=${pdb_path}
@@ -15,16 +15,13 @@ pdb_name=`basename ${pdb_path} .pdb`
 echo filename=${pdb_name}
 db=${rosetta_home}/rosetta_database
 echo db=${db}
-rosetta_scripts=${rosetta_home}/rosetta_source/bin/rosetta_scripts.linuxgcc${mode}
+rosetta_scripts=${rosetta_home}/rosetta_source/bin/rosetta_scripts.apbs.linuxgcc${mode}
 echo executable=${rosetta_scripts}
 
-echo I am here
+export LD_LIBRARY_PATH=${rosetta_home}/rosetta_source/external/apbs/apbs-1.4-rosetta/lib:$LD_LIBRARY_PATH
 
 #${rosetta_scripts} -database ${db}  -s ${path_name}/${pdb_name}.pdb  -parser:protocol ppk_minbb_use_correct.xml -ex1 -ex2 -use_input_sc -correct -overwrite
 
-echo here I am
+#${rosetta_scripts} -database ${db}  -s ${pdb_name}_0001.pdb  -parser:protocol ppk_minbb_add_elec.xml -ex1 -ex2 -use_input_sc -correct -parse_charge -overwrite
 
-${rosetta_scripts} -database ${db}  -s ${pdb_name}_0001.pdb  -parser:protocol ppk_minbb_add_elec.xml -ex1 -ex2 -use_input_sc -correct -parse_charge -overwrite
-
-echo here we go
-#${rosetta_scripts} -database ${db}  -s ${path_name}/${pdb_name}_0001_0001.pdb -parser:protocol scan_binding_with_elec.xml   -ex1 -ex2 -use_input_sc -nooutput -correct -parse_charge -overwrite
+${rosetta_scripts} -database ${db}  -s ${path_name}/${pdb_name}.pdb -parser:protocol scan_binding_with_elec.xml   -ex1 -ex2 -use_input_sc -nooutput -correct -parse_charge -overwrite
