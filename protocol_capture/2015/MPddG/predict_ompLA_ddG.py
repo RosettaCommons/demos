658108a9a8bb0661b557602832c89dc32b6a277e
Rebecca Alford <rfalford12@gmail.com>
2015-03-27 16:43:42 -0400

    Updating the specific script

--- protocol_capture/2015/MPddG/predict_ompLA_ddG.py
+++ protocol_capture/2015/MPddG/predict_ompLA_ddG.py
@@ -6,12 +6,13 @@
 # (c) For more information, see http://www.rosettacommons.org. Questions about this can be
 # (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
 
-## @file: compute_ompLA_ddG.py
+## @file: predict_ompLA_ddG.py
 ##
-## @brief: Compute ddG of mutation in ompLA 
-## @details: Use the Rosetta membrane framework to compute the ddG of unfolding of 
-## a membrane protein in Rosetta (uses packer, mutate.py from Evan Baugh) and compare
-## to experimentally meausred values in Moon & Fleming, 2011
+## @brief: Predict the ddG of mutation at position 181 of OmpLA
+## @details: Use the rosetta membrane framework (RosettaMP) to predict the ddG of 
+## mutation from alanine to all canonical residues at poisiton 181 of OmpLA. This
+## scripts uses the mutate_residue function, adapted from the mutate.py script in the 
+## 2012 PyRosetta workshops written by Evan Baugh. 
 ##
 ## Citation: Moon CP, Fleming KG (2011) Side-chain hydrophobicity scale derived from 
 ## transmembrane protein folding into lipid bilayers. Proc Natl Acad Sci. Available: 
@@ -24,15 +25,10 @@ from rosetta import *
 # tools
 import sys, os
 import commands
+import random
 
 # rosetta
 import rosetta.protocols.membrane
-
-## Default Values
-repack_radius = 0.0
-
-
-import random
 from rosetta import Pose
 from rosetta import create_score_function
 from rosetta import TaskFactory
@@ -41,17 +37,49 @@ from rosetta import aa_from_oneletter_code
 from rosetta import PackRotamersMover
 from rosetta.core.pose import PDBInfo
 
+## @brief:  Main Function for ddG Prediction
+## @details: Load in pose, setup membrane energy function, setup the membrane
+## framework, calculate ddGs 
+def main( argv ):
+
+    ## Step 1: Initialize Rosetta, including spanfile for the membrane framework
+    rosetta.init( extra_options="-mp:setup:spanfiles inputs/1qd6_tr_C.span -run:constant_seed" )
+
+    ## Step 2: Load in pose as a membrane pose (using AddMembraneMover)
+    pose = pose_from_pdb( "inputs/1qd6_tr_C.pdb" );
+    add_memb = rosetta.protocols.membrane.AddMembraneMover()
+    add_memb.apply( pose )
+    
+    ## Step 3: Orient Pose in the membrane based on transmembrane spans 
+    init_mem_pos = rosetta.protocols.membrane.MembranePositionFromTopologyMover()
+    init_mem_pos.apply( pose )
+
+    ## Step 4: Create a membrane energy function
+    ## @note: Two possible energy functions: (1) standard membrane full atom energy function
+    ## or (2) energy function accommodating for pH (requires extra flags - see README)
+    sfxn = create_score_function( "mpframework_smooth_fa_2012" );
+
+    ## Step 5: Repack neighbors within 8.0 angstroms of the mutant position
+    repacked_native = mutate_residue( pose, 181, 'A', 8.0 )
+
+    ## Step 6: Compute the ddG of mutation from alanine to each of the 20 canonical
+    ## amino acids. Then print the ddG file to an output file (ompLA_ddG.out)
+    AAs = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+    with file( 'ompLA_ddG.out' ) as f: 
+        for aa in AAs: 
+            ddG = compute_ddG( repacked_native, sfxn, 181, aa, 8.0 )
+            f.write( str(181) + " " + aa + " " + str( round( ddG, 3 ) ) + "\n" )
+    f.close
+
+## @brief Compute the ddG of mutation
 ## @brief Compute ddG of mutation in a protein at specified residue and AA position
-def compute_ddG( pose, sfxn, resnum, aa ): 
+def compute_ddG( pose, sfxn, resnum, aa, repack_radius ): 
 
     # Score Native Pose
     native_score = sfxn( pose )
 
     # Perform Mutation at residue <resnum> to amino accid <aa>
     mutated_pose = mutate_residue( pose, resnum, aa, repack_radius, sfxn )
-
-    # Adjust for pH
-    #adjust_for_pH( pose, resnum )
 
     # Score Mutated Pose
     mutant_score = sfxn( mutated_pose )
@@ -61,8 +89,7 @@ def compute_ddG( pose, sfxn, resnum, aa ):
 
 # @brief Replace the residue at <resid> in <pose> with <new_res> and allows
 # repacking within a given <pack_radius> 
-def mutate_residue( pose , mutant_position , mutant_aa ,
-        pack_radius = 0.0 , pack_scorefxn = '' ):
+def mutate_residue( pose, mutant_position, mutant_aa, pack_radius, sfxn ):
 
     if pose.is_fullatom() == False:
         IOError( 'mutate_residue only works with fullatom poses' )
@@ -70,13 +97,8 @@ def mutate_residue( pose , mutant_position , mutant_aa ,
     test_pose = Pose()
     test_pose.assign( pose )
 
-    # Create a talaris sfxn by default
-    if not pack_scorefxn:
-        pack_scorefxn = create_score_function( 'mpframework_smooth_fa_2012' )
-
     # Create a packer task (standard)
     task = TaskFactory.create_packer_task( test_pose )
-
 
     # the Vector1 of booleans (a specific object) is needed for specifying the
     #    mutation, this demonstrates another more direct method of setting
@@ -108,10 +130,10 @@ def mutate_residue( pose , mutant_position , mutant_aa ,
     # prevent residues from packing by setting the per-residue "options" of
     #    the PackerTask
     center = pose.residue( mutant_position ).nbr_atom_xyz()
-    for i in range( 1 , pose.total_residue() + 1 ):
+    for i in range( 1, pose.total_residue() + 1 ): 
+        dist = center.distance_squared( test_pose.residue( i ).nbr_atom_xyz() );  
         # only pack the mutating residue and any within the pack_radius
-        if not i == mutant_position or center.distance_squared(
-                test_pose.residue( i ).nbr_atom_xyz() ) > pack_radius**2:
+        if i != mutant_position and dist > pow( float( pack_radius ), 2 ) :
             task.nonconst_residue_task( i ).prevent_repacking()
 
     # apply the mutation and pack nearby residues
@@ -119,61 +141,6 @@ def mutate_residue( pose , mutant_position , mutant_aa ,
     packer.apply( test_pose )
 
     return test_pose
-
-## Protonate selected residues in the pose (for Moon & Fleming case only)
-def adjust_for_pH( pose, resnum ):
-
-    ## Create a copy of the pose to return
-    adjusted_pose = Pose()
-    adjusted_pose.assign( pose )
-
-    ## Apply residue variant type at the mutated position
-    add_variant_type_to_pose_residue( adjusted_pose, resnum, PROTONATED )
-
-    # Create a membrane framework pH scorefunction
-    pH_scorefxn = create_score_function( "mpframework_pHmode_fa_2015" )
-
-    # Create a packer task (standard)
-    task = TaskFactory.create_packer_task( adjusted_pose )
-
-    # Prevent all residues from repacking except for the 
-    # protonated residue by setting per-residue options
-    # of the packer task
-    for i in range( 1, adjusted_pose.total_residue() + 1 ): 
-        task.nonconst_residue_task( i ).prevent_repacking()
-
-    # Repack the protonated residue, restricting to protonated rotamers
-    # only
-    packer = PackRotamersMover( pH_scorefxn , task )
-    packer.apply( adjusted_pose )
-
-    return adjusted_pose
-
-## @brief Main - Add Membrane to Pose, Compute ddG
-def main( argv ):
-
-    rosetta.init( extra_options="-mp:setup:spanfiles inputs/1qd6_tr_C.span -run:constant_seed -in:ignore_unrecognized_res" )
-
-    # Load Pose, & turn on the membrane
-    pose = pose_from_pdb( "inputs/1qd6_tr_C.pdb" );
-    sfxn = create_score_function( "mpframework_smooth_fa_2012" );
-
-    #sfxn = create_score_function( "mpframework_pHmode_fa_2015" );
-    
-    # Add Membrane to Pose
-    add_memb = rosetta.protocols.membrane.AddMembraneMover()
-    add_memb.apply( pose )
-
-    # Setup in a topology based membrane
-    init_mem_pos = rosetta.protocols.membrane.MembranePositionFromTopologyMover()
-    init_mem_pos.apply( pose )
-
-    # Compute mutations 
-    #AAs = ['D', 'E']
-    AAs = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
-    ddGs = []
-    for aa in AAs:
-        print compute_ddG( pose, sfxn, 181, aa  )
 
 if __name__ == "__main__" : main(sys.argv)
 
