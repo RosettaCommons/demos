53a3e68662c5274e2da0a1ff2bc0cee974a64ff5
Kristin Blacklock <kmb413@rutgers.edu>
2016-06-21 05:54:45 -0700

    cleaning up, fixed minimization.md, added flags files and movemapfile

--- tutorials/clean.sh
+++
@@ -1,1 +0,0 @@
-/home/kmb413/Rosetta/main/source/bin/rosetta_scripts.linuxgccrelease -database /home/kmb413/Rosetta/main/database -parser:protocol /home/kmb413/empty.xml -s 3hon.pdb -score:weights talaris2014
