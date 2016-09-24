8b9101a0f56cd1b1cbb7c16cc4f1b399b62edd50
Andy Watkins <andy.watkins2@gmail.com>
2016-09-23 17:07:01 -0700

    New flags

--- public/recces/example_output/prerun/runall.sh
+++ public/recces/example_output/prerun/runall.sh
@@ -1,8 +1,8 @@
-    recces_turner -score:weights stepwise/rna/turner -n_cycle 300000 -seq1 gu -seq2 ac -temps 0.8 -out_prefix prerun &
-    recces_turner -score:weights stepwise/rna/turner -n_cycle 300000 -seq1 gu -seq2 ac -temps 1.0 -out_prefix prerun &
-    recces_turner -score:weights stepwise/rna/turner -n_cycle 300000 -seq1 gu -seq2 ac -temps 1.4 -out_prefix prerun &
-    recces_turner -score:weights stepwise/rna/turner -n_cycle 300000 -seq1 gu -seq2 ac -temps 1.8 -out_prefix prerun &
-    recces_turner -score:weights stepwise/rna/turner -n_cycle 300000 -seq1 gu -seq2 ac -temps 3.0 -out_prefix prerun &
-    recces_turner -score:weights stepwise/rna/turner -n_cycle 300000 -seq1 gu -seq2 ac -temps 7.0 -out_prefix prerun &
-    recces_turner -score:weights stepwise/rna/turner -n_cycle 300000 -seq1 gu -seq2 ac -temps 30  -out_prefix prerun &
+    recces_turner -score:weights stepwise/rna/turner -rna:farna:thermal_sampling:n_cycle 300000 -seq1 gu -seq2 ac -rna:farna:thermal_sampling:temps 0.8 -rna:farna:thermal_sampling:out_prefix prerun &
+    recces_turner -score:weights stepwise/rna/turner -rna:farna:thermal_sampling:n_cycle 300000 -seq1 gu -seq2 ac -rna:farna:thermal_sampling:temps 1.0 -rna:farna:thermal_sampling:out_prefix prerun &
+    recces_turner -score:weights stepwise/rna/turner -rna:farna:thermal_sampling:n_cycle 300000 -seq1 gu -seq2 ac -rna:farna:thermal_sampling:temps 1.4 -rna:farna:thermal_sampling:out_prefix prerun &
+    recces_turner -score:weights stepwise/rna/turner -rna:farna:thermal_sampling:n_cycle 300000 -seq1 gu -seq2 ac -rna:farna:thermal_sampling:temps 1.8 -rna:farna:thermal_sampling:out_prefix prerun &
+    recces_turner -score:weights stepwise/rna/turner -rna:farna:thermal_sampling:n_cycle 300000 -seq1 gu -seq2 ac -rna:farna:thermal_sampling:temps 3.0 -rna:farna:thermal_sampling:out_prefix prerun &
+    recces_turner -score:weights stepwise/rna/turner -rna:farna:thermal_sampling:n_cycle 300000 -seq1 gu -seq2 ac -rna:farna:thermal_sampling:temps 7.0 -rna:farna:thermal_sampling:out_prefix prerun &
+    recces_turner -score:weights stepwise/rna/turner -rna:farna:thermal_sampling:n_cycle 300000 -seq1 gu -seq2 ac -rna:farna:thermal_sampling:temps 30  -rna:farna:thermal_sampling:out_prefix prerun &
 
